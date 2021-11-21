e35cbee8135eafc1482e785a85d5c93a4779310f
William Villeneuve <ciiqr@users.noreply.github.com>
2021-11-21 00:49:02 -0500

    moved arg parsing to its own file

---
+++ src/args.rs
@@ -0,0 +1,50 @@
+use crate::extensions::{pico_args::PicoArgsExt, vec_os_string::VecOsStringToStringExt};
+
+#[derive(Debug)]
+pub struct Arguments {
+    pub global: GlobalArguments,
+    pub subcommand: Subcommand,
+}
+
+#[derive(Debug)]
+pub struct GlobalArguments {
+    pub help: bool,
+    pub version: bool,
+}
+
+#[derive(Debug)]
+pub enum Subcommand {
+    Provision { dry_run: bool },
+    Help,
+    Version,
+}
+
+pub fn parse_args() -> Result<Arguments, Box<dyn std::error::Error>> {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    let global = GlobalArguments {
+        help: pargs.contains_any(["-h", "--help"]),
+        version: pargs.contains_any(["-v", "--version"]),
+    };
+
+    let subcommand = match pargs.subcommand()?.as_deref() {
+        Some("p" | "provision") => Ok(Subcommand::Provision {
+            dry_run: pargs.contains_any("--dry-run"),
+        }),
+        Some("h" | "help") => Ok(Subcommand::Help),
+        Some("v" | "version") => Ok(Subcommand::Version),
+        Some(input) => Err(format!("unknown subcommand: {}", input)),
+        None => Ok(Subcommand::Help),
+    }?;
+
+    let remaining = pargs.finish();
+    if !remaining.is_empty() {
+        return Err(format!(
+            "unrecognized arguments: {}",
+            remaining.to_str_vec().join(" ")
+        )
+        .into());
+    }
+
+    return Ok(Arguments { global, subcommand });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use crate::extensions::{pico_args::PicoArgsExt, vec_os_string::VecOsStringToStringExt};
-use std::{error::Error, fmt::Debug};
+use args::{parse_args, Subcommand};
 
+mod args;
 mod extensions;
 
 const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -11,14 +11,6 @@ const USAGE: &str = indoc::indoc! {"
       nk h|help
       nk v|version
 "};
-
-fn help() {
-    println!("{}", USAGE);
-}
-
-fn version() {
-    println!("{}", VERSION);
-}
 
 fn main() {
     match parse_args() {
@@ -33,7 +25,7 @@ fn main() {
 
             match args.subcommand {
                 Subcommand::Provision { dry_run } => {
-                    println!("TODO: run provision subcommand with dry_run={}", dry_run)
+                    println!("TODO: implement provision: dry_run={}", dry_run)
                 }
                 Subcommand::Help => {
                     help();
@@ -50,54 +42,10 @@ fn main() {
     }
 }
 
-// TODO: move
-#[derive(Debug)]
-struct Arguments {
-    global: GlobalArguments,
-    subcommand: Subcommand,
+fn help() {
+    println!("{}", USAGE);
 }
 
-#[derive(Debug)]
-struct GlobalArguments {
-    help: bool,
-    version: bool,
-}
-
-#[derive(Debug)]
-enum Subcommand {
-    Provision { dry_run: bool },
-    Help,
-    Version,
-}
-
-fn parse_args() -> Result<Arguments, Box<dyn Error>> {
-    let mut pargs = pico_args::Arguments::from_env();
-
-    let global = GlobalArguments {
-        help: pargs.contains_any(["-h", "--help"]),
-        version: pargs.contains_any(["-v", "--version"]),
-    };
-
-    let res: Result<Subcommand, String> = match pargs.subcommand()?.as_deref() {
-        Some("p" | "provision") => Ok(Subcommand::Provision {
-            dry_run: pargs.contains_any("--dry-run"),
-        }),
-        Some("h" | "help") => Ok(Subcommand::Help),
-        Some("v" | "version") => Ok(Subcommand::Version),
-        Some(input) => Err(format!("unknown subcommand: {}", input).into()),
-        None => Ok(Subcommand::Help),
-    };
-    // TODO: this is kind of odd, with the second let... need a better way to define error typw
-    let subcommand = res?;
-
-    let remaining = pargs.finish();
-    if !remaining.is_empty() {
-        return Err(format!(
-            "unrecognized arguments: {}",
-            remaining.to_str_vec().join(" ")
-        )
-        .into());
-    }
-
-    return Ok(Arguments { global, subcommand });
+fn version() {
+    println!("{}", VERSION);
 }
