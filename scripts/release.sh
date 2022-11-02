434337f33ddb6e9d14841e61d071993c0356eff2
William Villeneuve <ciiqr@users.noreply.github.com>
2022-11-02 02:19:37 -0400

    largely untested release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,33 +2,54 @@
 
 set -e
 
-# TODO: make sure running on m1 darwin
-if [[ "$OSTYPE" != darwin* || "$(/usr/bin/uname -m)" != 'arm64' ]]; then
+# make sure running on arm/macos
+if [[ "$OSTYPE" != darwin* || "$(uname -m)" != 'arm64' ]]; then
     echo 'release: must be run from an apple silicon mac'
     exit 1
 fi
 
+# make sure git tags are up to date
+git fetch --tags
 
-# TODO: determine new version (bump minor)
+# get existing version
+declare existing_version
+existing_version="$(git tag -l --sort=-v:refname | head -1)"
 
-# TODO: update version in code
-# cargo set-version "$VERSION"
+# bump to new version
+declare version
+version="$(semver bump minor "$existing_version")"
 
-# TODO: commit & tag
-# git add Cargo.toml Cargo.lock
-# git commit -m "bumped version to ${VERSION}"
-# git tag "$VERSION"
-# git push origin "$VERSION"
+# update version in code
+cargo set-version "$version"
 
-# TODO: build macos arm
+# commit & tag
+git add Cargo.toml Cargo.lock
+git commit -m "bumped version to ${version}"
+declare tag="v${version}"
+git tag "$tag"
+git push origin "$tag"
+
+# build macos arm
+cargo build --release
 
 # TODO: wait for remote windows/linux builds
+declare output
+declare conclusion
+declare runId
+while [[ -n "$conclusion" ]]; do
+    output="$(gh run list -w .github/workflows/build.yml --branch "$tag" --json 'status,conclusion,databaseId' --jq '.[] | select(.status == "completed")' | head -1)"
+    conclusion="$(jq '.conclusion' <<< "$output")"
+    runId="$(jq '.databaseId' <<< "$output")"
+done
 
-# TODO: download remote builds
+# download remote builds
+# TODO: maybe download to a temp dir instead?
+gh run download "$runId" --dir './target/release'
 
-# TODO: create release
-# hub release create \
-#     -a 'nk-macos-aarch64' \
-#     -a 'nk-linux-x86_64' \
-#     -a 'nk-windows-x86_64.exe' \
-#     -m "$VERSION" "$VERSION"
+# create release
+gh release create \
+    --title "$tag" \
+    "$tag" \
+    './target/release/nk#nk-macos-aarch64' \
+    './target/release/nk-linux-x86_64#nk-linux-x86_64' \
+    './target/release/nk-windows-x86_64.exe/nk.exe#nk-windows-x86_64.exe'
